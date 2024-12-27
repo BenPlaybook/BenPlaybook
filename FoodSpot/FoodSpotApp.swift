ec14d1cdd40f73fedac03c2ba52ecc0bedf058ef
BenPlaybook <ben@playbooksystems.io>
2024-12-27 22:36:28 +0000

    Initial Commit

---
+++ FoodSpot/FoodSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSpotApp.swift
+//  FoodSpot
+//
+//  Created by Ben Robinson on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodSpotTests/FoodSpotTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSpotTests.swift
+//  FoodSpotTests
+//
+//  Created by Ben Robinson on 27/12/2024.
+//
+
+import Testing
+@testable import FoodSpot
+
+struct FoodSpotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
